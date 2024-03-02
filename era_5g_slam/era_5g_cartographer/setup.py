7e178ef3bf9363b69682e8beb9b29e8eb63642bc
Michal Kapinus <michal.kapinus@gmail.com>
2024-03-02 19:10:22 +0100

    feat: updated config files

--- era_5g_slam/era_5g_cartographer/setup.py
+++ era_5g_slam/era_5g_cartographer/setup.py
@@ -1,3 +1,5 @@
+from glob import glob
+import os
 from setuptools import find_packages, setup
 
 package_name = 'era_5g_cartographer'
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*lua')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,

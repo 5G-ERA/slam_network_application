8482f201d643ad256deb138078747b011ce6783a
Michal Kapinus <michal.kapinus@gmail.com>
2024-06-02 21:38:24 +0200

    feat: updated configs launch files for summit added asset_writer config and launch

--- era_5g_slam/era_5g_cartographer/config/mapping.summitxl.lua
+++ era_5g_slam/era_5g_cartographer/config/mapping.summitxl.lua
@@ -1,6 +1,7 @@
 include "map_builder.lua"
 include "trajectory_builder.lua"
 
+
 options = {
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
@@ -10,6 +11,7 @@ options = {
   odom_frame = "robot/odom",
   provide_odom_frame = false,
   publish_frame_projected_to_2d = true,
+  use_pose_extrapolator = true,
   use_odometry = true,
   use_nav_sat = false,
   use_landmarks = false,
@@ -30,29 +32,32 @@ options = {
 
 TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 1
 
+-- MAP_BUILDER.use_trajectory_builder_3d = true
+-- MAP_BUILDER.num_background_threads = 7
+-- POSE_GRAPH.optimization_problem.huber_scale = 5e2
+-- POSE_GRAPH.optimize_every_n_nodes = 100
+-- POSE_GRAPH.constraint_builder.sampling_ratio = 0.03
+-- POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 5 --10
+-- POSE_GRAPH.constraint_builder.min_score = 0.4 -- 0.5
+-- POSE_GRAPH.constraint_builder.global_localization_min_score = 0.45 --0.55
+-- TRAJECTORY_BUILDER_3D.ceres_scan_matcher.translation_weight = 4e2 --5 for sim because gazebo translation does not reflect correctly
+-- TRAJECTORY_BUILDER_3D.ceres_scan_matcher.rotation_weight = 4e2 --5 for sim because gazebo rotation does not reflect correctly
+
 MAP_BUILDER.use_trajectory_builder_3d = true
 MAP_BUILDER.num_background_threads = 7
-POSE_GRAPH.optimization_problem.huber_scale = 5e2
 POSE_GRAPH.optimize_every_n_nodes = 60
-POSE_GRAPH.constraint_builder.sampling_ratio = 0.03
-POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 10
-POSE_GRAPH.constraint_builder.min_score = 0.6
-POSE_GRAPH.constraint_builder.global_localization_min_score = 0.65
-
--- Only sampling_ratio * total_nodes_in_search_window will be considered to create matches
--- Decrease to reduce CPU usage, increase to increment potential matches between nodes
 POSE_GRAPH.constraint_builder.sampling_ratio = 0.1
-
--- Sets the weight of odometry in pose estimation
--- Reduce to increase scan_matcher belief (position estimation will jitter with very small values)
--- Reduce in enviroments where odometry can't be trusted
-TRAJECTORY_BUILDER_3D.ceres_scan_matcher.translation_weight = 500
-TRAJECTORY_BUILDER_3D.ceres_scan_matcher.rotation_weight = 700
-
--- Setting them to true is creating problems even in 2D environments. Keep them deactivated
-POSE_GRAPH.constraint_builder.ceres_scan_matcher_3d.only_optimize_yaw = false
-TRAJECTORY_BUILDER_3D.ceres_scan_matcher.only_optimize_yaw = false
+--POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 10
 POSE_GRAPH.optimization_problem.use_online_imu_extrinsics_in_3d = false
-POSE_GRAPH.optimization_problem.fix_z_in_3d = false
-
+POSE_GRAPH.constraint_builder.min_score = 0.65
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.70
+TRAJECTORY_BUILDER_3D.ceres_scan_matcher.translation_weight = 700
+TRAJECTORY_BUILDER_3D.ceres_scan_matcher.rotation_weight = 1000
+TRAJECTORY_BUILDER_3D.voxel_filter_size = 0.1
+TRAJECTORY_BUILDER_3D.submaps.num_range_data = 80
+TRAJECTORY_BUILDER_3D.submaps.low_resolution = 0.25
+TRAJECTORY_BUILDER_3D.submaps.high_resolution = 0.15
+POSE_GRAPH.constraint_builder.ceres_scan_matcher_3d.only_optimize_yaw = true
+TRAJECTORY_BUILDER_3D.ceres_scan_matcher.only_optimize_yaw = true
+POSE_GRAPH.optimization_problem.fix_z_in_3d = true
 return options
---
+++ era_5g_slam/era_5g_cartographer/launch/asset_writer.launch.py
@@ -0,0 +1,27 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+
+use_sim_time = True
+use_sim_time_param = [{'use_sim_time': use_sim_time}]
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument('config', default_value="asset_writer_ros.lua", description='The configuration file for the Summit XL robot.'),
+        DeclareLaunchArgument('bag_name', description='The name of the bagfile.'),
+        DeclareLaunchArgument('pbstream_file', description='The name of the bagfile.'),
+        Node(
+            package='cartographer_ros',
+            executable='cartographer_assets_writer',
+            name='assets_writer_node',
+            parameters=use_sim_time_param,
+            arguments=["-configuration_directory", PathJoinSubstitution([FindPackageShare('era_5g_cartographer'), 'config']), 
+                       "-configuration_basename", LaunchConfiguration('config'),
+                       "-bag_filenames", LaunchConfiguration('bag_name'),
+                       "-pose_graph_filename", LaunchConfiguration('pbstream_file'),]
+            
+        ),     
+    ])
